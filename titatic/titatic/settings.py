551c6f4e63c39ae6ae776fb9723e4b4cf3dd1274
shobu8888 <sachin.gupta11jan@gmail.com>
2021-02-17 16:43:44 -0500

    added css and js

--- titatic/titatic/settings.py
+++ titatic/titatic/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'uuqe*7f1y!w3xk*de59#r@ib-#u)62r)%&28-i)ygqs7j5f9%)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['13.58.141.65','127.0.0.1']
 
 
 # Application definition
@@ -116,5 +116,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
+import os.path
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join('static'),)
